6f1c4ecc5c0b398de38cfbe9b96f49c4a05b570f
sebastian26 <sebastian@fluz.app>
2019-06-12 22:19:57 -0400

    stub out Deck component

---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react-native'
+import {View, Animated} from 'react-native'
+
+class Deck extends Component {
+  render() {
+    return (
+      <View />
+    )
+  }
+}
+
+export default Deck;
